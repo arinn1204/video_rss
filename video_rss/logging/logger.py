6899ffd2969dc2e26cb98e80cfcaac055817d899
Aaron Rinn <arinn1250@gmail.com>
2020-01-04 10:07:13 -0800

    creating initial logger

--- video_rss/logging/logger.py
+++ video_rss/logging/logger.py
@@ -1,3 +1,21 @@
+#!.env/bin/python
+
+
 class Logger:
     def __init__(self, config):
-        self.config = config
+        levels = {
+            1: 'TRACE',
+            2: 'DEBUG',
+            3: 'WARNING',
+            4: 'ERROR',
+            5: 'CRITICAL',
+            6: 'FATAL'
+        }
+        self.log_level = levels[config.logging_severity]
+
+    def log_trace(self, log):
+        if self.log_level <= 1:
+            self.__log(log)
+
+    def __log(self, log):
+        pass
