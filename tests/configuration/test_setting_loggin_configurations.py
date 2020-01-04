4e1e40f7110923cd88c3e6ff2d0dc9afbf63198a
Aaron Rinn <arinn1250@gmail.com>
2020-01-04 09:59:58 -0800

    added logging severity to configuration

---
+++ tests/configuration/test_setting_loggin_configurations.py
@@ -0,0 +1,36 @@
+#!.env/bin/python
+
+import os
+import re
+
+from ...video_rss import configuration
+
+
+class TestSettingLoggingConfigurations:
+    def teardown_method(self, method):
+        for key in os.environ.keys():
+            if bool(re.match('^logging', key, re.I)):
+                os.environ.pop(key, None)
+
+    def test_sets_default_log_severity_to_debug(self):
+        config = configuration.Configuration()
+
+        severity = config.logging_severity
+
+        assert severity == 'DEBUG'
+
+    def test_sets_log_severity_from_environment(self):
+        os.environ['logging_severity'] = 'TRACE'
+        config = configuration.Configuration()
+
+        severity = config.logging_severity
+
+        assert severity == 'TRACE'
+
+    def test_sets_log_severity_from_environment_and_sets_to_upper(self):
+        os.environ['logging_severity'] = 'trace'
+        config = configuration.Configuration()
+
+        severity = config.logging_severity
+
+        assert severity == 'TRACE'
--- video_rss/configuration.py
+++ video_rss/configuration.py
@@ -29,9 +29,19 @@ class Configuration:
         self.__default_assignment(
             'database_instance',
             None)
+        self.__default_assignment('logging_severity', 'DEBUG', True)
 
-    def __default_assignment(self, property_name, default_value):
+    def __default_assignment(
+            self,
+            property_name,
+            default_value,
+            to_upper=False):
+        value = default_value
         if property_name in environ:
-            self.__dict__[property_name] = environ[property_name]
-        elif default_value is not None:
-            self.__dict__[property_name] = default_value
+            value = environ[property_name]
+
+        if to_upper:
+            value = value.upper()
+
+        if value is not None:
+            self.__dict__[property_name] = value
