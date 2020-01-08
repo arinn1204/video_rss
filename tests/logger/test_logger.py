d3a0283c379b5864ba7fc1c6946b843347cfad0c
Aaron Rinn <arinn1250@gmail.com>
2020-01-07 18:48:24 -0800

    added configurable masker to logger to prevent sensitive information from being logged

---
+++ tests/logger/test_logger.py
@@ -0,0 +1,91 @@
+#!.env/bin/python
+
+from ...video_rss import configuration
+from ...video_rss.logging.logger import Logger
+
+
+class TestLogger:
+
+    def setup_method(self, method):
+        self.config = configuration.Configuration()
+        self.config.logging_severity = 'TRACE'
+
+    def test_should_pass_log_object_to_json_converter(self, mocker):
+        self.config.__dict__.clear()
+        self.config.logging_severity = 'TRACE'
+        self.config.logging_masked_values = ['password', 'key']
+
+        log_object = {
+            'username': 'hunter2',
+            'password': '***'
+        }
+
+        expected_result = {
+            'message': {
+                'username': 'hunter2',
+                'password': '***'
+            },
+            'logging': {
+                'severity': 'TRACE',
+                'masked_values': ['password', 'key']
+            }
+        }
+
+        json_mock = mocker.patch('json.dumps')
+        logger = Logger(self.config)
+
+        logger.log_trace(log_object)
+
+        json_mock.assert_called_once_with(expected_result)
+
+    def test_should_mask_database_password(self, mocker):
+        self.config.__dict__.clear()
+        self.config.logging_severity = 'TRACE'
+        self.config.database_password = 'hunter2'
+        self.config.logging_masked_values = ['password']
+
+        log_object = "some log message oh no!"
+
+        expected_result = {
+            'message': "some log message oh no!",
+            'logging': {
+                'severity': 'TRACE',
+                'masked_values': ['password']
+            },
+            'database': {
+                'password': '********'
+            }
+        }
+
+        json_mock = mocker.patch('json.dumps')
+        logger = Logger(self.config)
+
+        logger.log_trace(log_object)
+
+        json_mock.assert_called_once_with(expected_result)
+
+    def test_should_mask_api_key(self, mocker):
+        self.config.__dict__.clear()
+        self.config.logging_severity = 'TRACE'
+        self.config.someapi_key = 'hunter2'
+        self.config.logging_masked_values = ['key']
+
+        log_object = "some log message oh no!"
+
+        expected_result = {
+            'message': "some log message oh no!",
+            'logging': {
+                'severity': 'TRACE',
+                'masked_values': ['key']
+            },
+            'someapi': {
+                'key': '********'
+            }
+        }
+
+        json_mock = mocker.patch('json.dumps')
+        logger = Logger(self.config)
+
+        logger.log_trace(log_object)
+
+        json_mock.assert_called_once_with(expected_result)
--- video_rss/logging/logger.py
+++ video_rss/logging/logger.py
@@ -1,21 +1,53 @@
 #!.env/bin/python
 
+import json
+
 
 class Logger:
     def __init__(self, config):
         levels = {
-            1: 'TRACE',
-            2: 'DEBUG',
-            3: 'WARNING',
-            4: 'ERROR',
-            5: 'CRITICAL',
-            6: 'FATAL'
+            'OFF': -1,
+            'FATAL': 1,
+            'CRITICAL': 2,
+            'ERROR': 3,
+            'WARNING': 4,
+            'DEBUG': 5,
+            'TRACE': 6,
+            'ALL': 99
         }
         self.log_level = levels[config.logging_severity]
+        self.build_log = lambda message: self.__build_log(message, config)
+        self.masked_values = config.logging_masked_values
 
     def log_trace(self, log):
-        if self.log_level <= 1:
+        if self.log_level >= 6:
             self.__log(log)
 
-    def __log(self, log):
-        pass
+    def __log(self, message):
+        log = self.build_log(message)
+        formatted_log = json.dumps(log)
+        print(formatted_log)
+
+    def __build_log(self, log, config):
+        new_log = {
+            'message': log
+        }
+        for key, value in config.__dict__.items():
+            config_sections = key.split('_', 1)
+            category = config_sections[0]
+            config_value = config_sections[1]
+
+            if category not in new_log:
+                new_log[category] = {}
+
+            log_category = new_log[category]
+
+            log_category[config_value] = self.__mask(config_value, value)
+
+        return new_log
+
+    def __mask(self, key, value):
+        if key in self.masked_values:
+            return '*' * 8
+        else:
+            return value
