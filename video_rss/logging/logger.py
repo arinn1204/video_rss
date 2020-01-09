e729be69660d46698df479a20db2ea90971773ae
Aaron Rinn <arinn1250@gmail.com>
2020-01-08 18:12:22 -0800

    refactoring private variables; cleaning up

--- video_rss/logging/logger.py
+++ video_rss/logging/logger.py
@@ -5,7 +5,40 @@ import json
 
 class Logger:
     def __init__(self, config):
-        self.levels = {
+        self.log_level = self.__log_value(config.logging_severity)
+        self.build_log = lambda message: self.__build_log(message, config)
+        self.masked_values = config.logging_masked_values
+
+    def log(self, message, severity='TRACE'):
+        if self.log_level >= self.__log_value(severity):
+            log = self.build_log(message)
+            formatted_log = json.dumps(log)
+            print(formatted_log)
+
+    def __build_log(self, log, config):
+        new_log = {
+            'message': log
+        }
+        for key, value in config.__dict__.items():
+            section, config_name = key.split('_', 1)
+
+            if section not in new_log:
+                new_log[section] = {}
+
+            log_category = new_log[section]
+
+            log_category[config_name] = self.__mask(config_name, value)
+
+        return new_log
+
+    def __mask(self, key, value):
+        if key in self.masked_values:
+            return '*' * 8
+        else:
+            return value
+
+    def __log_value(self, level_string):
+        levels = {
             'OFF': -1,
             'FATAL': 1,
             'CRITICAL': 2,
@@ -15,39 +48,5 @@ class Logger:
             'TRACE': 6,
             'ALL': 99
         }
-        self.log_level = self.levels[config.logging_severity]
-        self.build_log = lambda message: self.__build_log(message, config)
-        self.masked_values = config.logging_masked_values
 
-    def log(self, log, severity='TRACE'):
-        if self.log_level >= self.levels[severity.upper()]:
-            self.__log(log)
-
-    def __log(self, message):
-        log = self.build_log(message)
-        formatted_log = json.dumps(log)
-        print(formatted_log)
-
-    def __build_log(self, log, config):
-        new_log = {
-            'message': log
-        }
-        for key, value in config.__dict__.items():
-            config_sections = key.split('_', 1)
-            category = config_sections[0]
-            config_value = config_sections[1]
-
-            if category not in new_log:
-                new_log[category] = {}
-
-            log_category = new_log[category]
-
-            log_category[config_value] = self.__mask(config_value, value)
-
-        return new_log
-
-    def __mask(self, key, value):
-        if key in self.masked_values:
-            return '*' * 8
-        else:
-            return value
+        return levels[level_string.upper()]
