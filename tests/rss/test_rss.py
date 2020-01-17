a9be642f3cb0e7bd4e11a2650603b73dd9567243
Aaron Rinn <arinn1250@gmail.com>
2020-01-16 18:42:12 -0800

    ignoring all line length violations in file

--- tests/rss/test_rss.py
+++ tests/rss/test_rss.py
@@ -1,4 +1,5 @@
 #!.env/bin/python
+# flake8: noqa
 
 from ...video_rss.rss import rss as sut
 from ...video_rss import configuration
@@ -14,7 +15,7 @@ def test_should_parse_feed_from_config(mocker):
     rss = sut.Rss(config, logger)
     _ = [i for i in rss.get_videos()]
 
-    feedparser.assert_called_once_with('showrss.info/test_should_parse_feed_from_config')  # noqa: E501
+    feedparser.assert_called_once_with('showrss.info/test_should_parse_feed_from_config')
 
 
 def test_should_retrieve_all_rss_objects(mocker):
@@ -30,5 +31,5 @@ def test_should_retrieve_all_rss_objects(mocker):
 
     assert data[0] == {
         'title': 'Vikings S06E07 720p WEB H264 GHOSTS',
-        'magnet': 'magnet:?xt=urn:btih:743E0F4656DFD378420C6C8B9127121B1D656E3A&dn=Vikings+S06E07+720p+WEB+H264+GHOSTS&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce',  # noqa: E501
+        'magnet': 'magnet:?xt=urn:btih:743E0F4656DFD378420C6C8B9127121B1D656E3A&dn=Vikings+S06E07+720p+WEB+H264+GHOSTS&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce',
         'id': 'cc4a6cca13da8d2483e88c2e640027857aef64a7'}
