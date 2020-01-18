56241b5418229f2c955287e533377e41c0d04b4d
Aaron Rinn <arinn1250@gmail.com>
2020-01-18 10:03:16 -0800

    added test ensuring it is reading all items not just one from rss

--- tests/rss/rss_samples.py
+++ tests/rss/rss_samples.py
@@ -18,6 +18,12 @@ def get_showrss_sample_with_one_entry():
         data = json.loads(json_data)
     return data
 
+def get_showrss_sample_with_two_entries():
+    with open(_get_path('showrss_two_items.json'), 'r') as file:
+        json_data = file.read()
+        data = json.loads(json_data)
+    return data
+
 def _get_path(path):
     current_path = os.path.dirname(os.path.abspath(__file__))
     return current_path + os.path.sep + path
--- tests/rss/test_rss.py
+++ tests/rss/test_rss.py
@@ -5,6 +5,7 @@ from ...video_rss.rss import rss as sut
 from ...video_rss import configuration
 
 from .rss_samples import get_showrss_sample_with_one_entry
+from .rss_samples import get_showrss_sample_with_two_entries
 
 
 def test_should_parse_feed_from_config(mocker):
@@ -18,7 +19,7 @@ def test_should_parse_feed_from_config(mocker):
     feedparser.assert_called_once_with('showrss.info/test_should_parse_feed_from_config')
 
 
-def test_should_retrieve_all_rss_objects(mocker):
+def test_should_retrieve_rss_objects_in_list(mocker):
     feedparser = mocker.patch('feedparser.parse')
     feedparser.return_value = get_showrss_sample_with_one_entry()
     logger = mocker.Mock()
@@ -29,7 +30,27 @@ def test_should_retrieve_all_rss_objects(mocker):
     data = [i for i in rss.get_videos()]
     assert len(data) == 1
 
-    assert data[0] == {
+    assert data == [{
         'title': 'Vikings S06E07 720p WEB H264 GHOSTS',
         'magnet': 'magnet:?xt=urn:btih:743E0F4656DFD378420C6C8B9127121B1D656E3A&dn=Vikings+S06E07+720p+WEB+H264+GHOSTS&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce',
-        'id': 'cc4a6cca13da8d2483e88c2e640027857aef64a7'}
+        'id': 'cc4a6cca13da8d2483e88c2e640027857aef64a7'}]
+
+def test_should_retrieve_all_rss_objects(mocker):
+    feedparser = mocker.patch('feedparser.parse')
+    feedparser.return_value = get_showrss_sample_with_two_entries()
+    logger = mocker.Mock()
+    config = configuration.Configuration()
+    config.rss_feed_address = 'showrss.info/user'
+    rss = sut.Rss(config, logger)
+
+    data = [i for i in rss.get_videos()]
+    assert len(data) == 2
+
+    assert data == [{
+        'title': 'Vikings S06E07 720p WEB H264 GHOSTS',
+        'magnet': 'magnet:?xt=urn:btih:743E0F4656DFD378420C6C8B9127121B1D656E3A&dn=Vikings+S06E07+720p+WEB+H264+GHOSTS&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce',
+        'id': 'cc4a6cca13da8d2483e88c2e640027857aef64a7'},
+        {
+        'title': 'DCs Legends of Tomorrow S05E01 720p HDTV x264 SVA',
+        'magnet': 'magnet:?xt=urn:btih:9CD90E920932126815F826A007670365F48F7DCC&dn=DCs+Legends+of+Tomorrow+S05E01+720p+HDTV+x264+SVA&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce',
+        'id': '68312bf69c0c91e6fcfd82ea0c2988a51f18a140'}]
