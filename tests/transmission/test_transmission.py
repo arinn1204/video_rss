95c56079cff4633ddd4f45adafa9897828881b57
Aaron Rinn <arinn1250@gmail.com>
2020-01-20 10:13:47 -0800

    added ability to get new session id if the post failed and to retry adding the magnet one time

--- tests/transmission/test_transmission.py
+++ tests/transmission/test_transmission.py
@@ -6,6 +6,7 @@ from ...video_rss.configuration import Configuration
 from . import helpers
 import uuid
 from unittest import mock
+from unittest.mock import call
 from json import dumps
 
 
@@ -29,6 +30,28 @@ def test_should_add_supplied_torrent(mocker):
                     'X-Transmission-Session-Id': fake_session_id
                 },
                 auth=('username', 'password'))
+
+
+def test_should_get_new_session_id_if_409_returned(mocker):
+    with mock.patch(f"{session_id.__name__}.get_session_id") as mocked_session:
+        fake_session_id = str(uuid.uuid4())
+        mocked_session.return_value = fake_session_id
+        logger = mocker.Mock()
+        get_session = call('url', ('username', 'password'), logger)
+
+        transmission = Transmission(build_config(), logger)
+
+        with mock.patch('requests.post') as mocked_request:
+            fake_request = helpers.sample_request_session_id()
+            fake_request.status_code = 409
+
+            mocked_request.return_value = fake_request
+
+            transmission.add_torrent('magnet://')
+
+            mocked_session.assert_has_calls([
+                get_session, get_session
+            ])
 
 
 def build_config():
--- video_rss/transmission/transmission.py
+++ video_rss/transmission/transmission.py
@@ -14,14 +14,19 @@ class Transmission:
         self.session_id = session_id.get_session_id(
             config.transmission_url, self.auth, logger)
 
-    def add_torrent(self, magnet):
+    def add_torrent(self, magnet, retry=True):
         body = self.__build_payload(magnet)
         headers = self.__build_headers()
-        requests.post(
+        response = requests.post(
             self.config.transmission_url,
             body,
             headers=headers,
             auth=self.auth)
+
+        if response.status_code == 409 and retry:
+            self.session_id = session_id.get_session_id(
+                self.config.transmission_url, self.auth, self.logger)
+            self.add_torrent(magnet, retry=False)
 
     def __build_headers(self):
         return {
