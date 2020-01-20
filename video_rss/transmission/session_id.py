b592fd9ecc9e6bdd4f83ac8c7e92489df7d3bc63
Aaron Rinn <arinn1250@gmail.com>
2020-01-19 21:21:44 -0800

    added optional logging

--- video_rss/transmission/session_id.py
+++ video_rss/transmission/session_id.py
@@ -4,8 +4,12 @@ import requests
 from lxml import html
 
 
-def get_session_id(url, username, password):
+def get_session_id(url, username, password, logger=None):
     response = requests.get(url, auth=(username, password))
+
+    if logger is not None:
+        logger.log(response.text, 'DEBUG')
+
     response_tree = html.fromstring(response.text)
     id_value = response_tree.xpath('//code/text()')[0]
     return id_value.split(':')[1].strip()
