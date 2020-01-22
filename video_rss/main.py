e1ade02e22c3c9db2a0fceffc339c2a144975891
Aaron Rinn <arinn1250@gmail.com>
2020-01-21 19:14:51 -0800

    added implementation to main method, added new wrapper module

--- video_rss/main.py
+++ video_rss/main.py
@@ -1,3 +1,36 @@
 #!.env/bin/python
 
-print('Hello, World!')
+import argparse
+import datetime
+from torrents import transmission
+from time import sleep
+
+
+def main(arguments):
+    interval = datetime.timedelta(0, arguments.interval)
+    stored_time = datetime.datetime(2000, 1, 1)
+
+    while True:
+        if stored_time + interval <= datetime.datetime.now():
+            transmission.add_torrent()
+            stored_time = datetime.datetime.now()
+        else:
+            time_remain = stored_time + interval - datetime.datetime.now()
+            sleep(time_remain.total_seconds())
+
+
+if __name__ == '__main__':
+    description = 'Process a given RSS torrent feed and add any new torrents'
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        '-i',
+        '--interval',
+        default=15,
+        dest='interval',
+        action='store',
+        help='defines the polling interval in seconds',
+        type=int
+        )
+
+    main(parser.parse_args())
---
+++ video_rss/torrents/transmission.py
@@ -0,0 +1,7 @@
+#!.env/bin/python
+
+from datetime import datetime
+
+
+def add_torrent():
+    print(f"Hello from {datetime.now()}!")
