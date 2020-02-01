e88dbeab0bf1754aa9277cb371a26a6537ee551f
Aaron Rinn <arinn1250@gmail.com>
2020-01-31 20:04:19 -0800

    using module to define mock instead of module path

--- tests/logger/test_logger.py
+++ tests/logger/test_logger.py
@@ -1,7 +1,7 @@
 #!.env/bin/python
 
 from ...video_rss import configuration
-from ...video_rss.logging.logger import Logger
+from ...video_rss.logging import logger as logger_module
 import datetime
 
 from unittest import mock
@@ -9,7 +9,7 @@ from unittest import mock
 
 def test_should_pass_log_object_to_json_converter(mocker):
     stubbed_time = str(datetime.datetime(2020, 2, 2))
-    with mock.patch('video_rss.video_rss.logging.logger.datetime.datetime') as mock_date:  # noqa: E501
+    with mock.patch(f'{logger_module.__name__}.datetime.datetime') as mock_date:  # noqa: E501
         mock_date.now.return_value = stubbed_time
 
         config = configuration.Configuration()
@@ -35,7 +35,7 @@ def test_should_pass_log_object_to_json_converter(mocker):
         }
 
         json_mock = mocker.patch('json.dumps')
-        logger = Logger(config)
+        logger = logger_module.Logger(config)
 
         logger.log(log_object)
 
@@ -44,7 +44,7 @@ def test_should_pass_log_object_to_json_converter(mocker):
 
 def test_should_mask_database_password(mocker):
     stubbed_time = str(datetime.datetime(2020, 2, 2))
-    with mock.patch('video_rss.video_rss.logging.logger.datetime.datetime') as mock_date:  # noqa: E501
+    with mock.patch(f'{logger_module.__name__}.datetime.datetime') as mock_date:  # noqa: E501
         mock_date.now.return_value = stubbed_time
         config = configuration.Configuration()
         config.__dict__.clear()
@@ -67,7 +67,7 @@ def test_should_mask_database_password(mocker):
         }
 
         json_mock = mocker.patch('json.dumps')
-        logger = Logger(config)
+        logger = logger_module.Logger(config)
 
         logger.log(log_object)
 
@@ -76,7 +76,7 @@ def test_should_mask_database_password(mocker):
 
 def test_should_mask_api_key(mocker):
     stubbed_time = str(datetime.datetime(2020, 2, 2))
-    with mock.patch('video_rss.video_rss.logging.logger.datetime.datetime') as mock_date:  # noqa: E501
+    with mock.patch(f'{logger_module.__name__}.datetime.datetime') as mock_date:  # noqa: E501
         mock_date.now.return_value = stubbed_time
         config = configuration.Configuration()
         config.__dict__.clear()
@@ -99,7 +99,7 @@ def test_should_mask_api_key(mocker):
         }
 
         json_mock = mocker.patch('json.dumps')
-        logger = Logger(config)
+        logger = logger_module.Logger(config)
 
         logger.log(log_object)
 
@@ -115,7 +115,7 @@ def test_should_not_log_if_below_threshold(mocker):
     log_object = "some log message oh no!"
 
     json_mock = mocker.patch('json.dumps')
-    logger = Logger(config)
+    logger = logger_module.Logger(config)
 
     logger.log(log_object, 'TRACE')
 
