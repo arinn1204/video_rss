baffec2997d8771106620ffcabc10403095bb6a5
Aaron Rinn <arinn1250@gmail.com>
2020-02-03 18:33:10 -0800

    made mock setup method more module

--- tests/database/database_helpers.py
+++ tests/database/database_helpers.py
@@ -1,7 +1,7 @@
 #!.env/bin/python
 
 
-def setup_mocks(mocker):
+def setup_mocks(mocker, fetchall=[(1,)], rowcount=1):
     mocker.patch('sys.platform').return_value = 'Windows'
     mock_pyodbc_connection = mocker.Mock()
     mock_connection = mocker.Mock()
@@ -12,7 +12,8 @@ def setup_mocks(mocker):
     mock_pyodbc_connection.return_value = mock_connection
     mock_connection.cursor.return_value = mock_cursor
     mock_cursor.execute.return_value = mock_execute
-    mock_execute.fetchall.return_value = [(1,)]
-    mock_execute.rowcount = 1
+
+    mock_execute.fetchall.return_value = fetchall
+    mock_execute.rowcount = rowcount
 
     return (mock_connection, mock_cursor, mock_execute)
